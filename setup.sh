f0a9a63b0e1219528ab53d04dfab45233a40bfc1
潘岳 <panyue05@corp.netease.com>
2020-09-01 14:51:12 +0800

    test

--- setup.sh
+++ setup.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 image_version=`date +%Y%m%d%H%M`;
-# 关闭shop_admin容器
-docker stop shop_admin || true;
-# 删除shop_admin容器
-docker rm shop_admin || true;
-# 删除shop/admin镜像
-docker rmi --force $(docker images | grep shop/admin | awk '{print $3}')
-# 构建shop/admin:$image_version镜像
-docker build . -t shop/admin:$image_version;
+# 关闭a_test容器
+docker stop a_test || true;
+# 删除a_test容器
+docker rm a_test || true;
+# 删除a/test镜像
+docker rmi --force $(docker images | grep a/test | awk '{print $3}')
+# 构建a/test:$image_version镜像
+docker build . -t a/test:$image_version;
 # 查看镜像列表
 docker images;
-# 基于shop/admin 镜像 构建一个容器 shop_admin
-docker run -p 9527:80 -d --name shop_admin shop/admin:$image_version;
+# 基于a/test 镜像 构建一个容器 a_test
+docker run -p 9527:80 -d --name a_test a/test:$image_version;
 # 查看日志
-docker logs shop_admin;
+docker logs a_test;
 #删除build过程中产生的镜像    #docker image prune -a -f
 docker rmi $(docker images -f "dangling=true" -q)
 # 对空间进行自动清理
